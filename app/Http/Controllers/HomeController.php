2b85c85be966b2ff390b1bbd7dc9ce427ca69b43
brokiki <kilian.debraux@lacatholille.fr>
2020-03-05 09:11:23 +0100

    maj recettes listes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -22,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $recettes = DB::select('SELECT r.id, r.titre, r.auteur auteur_id, r.text, r.created_at, r.updated_at, u.name auteur FROM recettes r INNER JOIN users u ON r.auteur = u.id');
+        return view('home', compact('recettes'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 @extends ('page')
 
 @section('body')
-<h2>A faire</h2>
+<div class="card-columns border border-primary rounded">
+    @foreach($recettes as $recette)
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">{{ $recette['titre'] }}</h5>
+                <p class="card-text">{{ $recette['text'] }}</p>
+            </div>
+        </div>
+    @endforeach
+</div>
 @endsection
