b1b7fc23d3ccc0eff332a6a9216863c50ddff735
chewbaccademy <kilian.debraux@lacatholille.fr>
2020-04-01 10:27:55 +0200

    nouvelle table + tests fontionnent !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- database/factories/RecetteFactory.php
+++ database/factories/RecetteFactory.php
@@ -11,12 +11,19 @@ $factory->define(\App\Recette::class, function (Faker $faker) {
     for ($i = 0; $i < $nb; $i++) {
         $arrPhotos[$i] = $faker->imageUrl ();
     }
+
+    $nb = $faker->randomDigit;
+    $arrNb = [];
+    for ($i = 0; $i < $nb; $i++) {
+        $arrNb[$i] = $faker->randomDigit;
+    }
+    
     return [
         'text' => $faker->paragraph,
         'titre' =>$faker->text (255),
         'photoUrls' => $faker->sentences($faker->randomDigit),
-        'ingredients' => $faker->words($faker->randomDigit),
-        'categories' => $faker->words($faker->randomDigit)
+        'ingredients' => $arrNb,
+        'categories' => $arrNb
 
     ];
 });
